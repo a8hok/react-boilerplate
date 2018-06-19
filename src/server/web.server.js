1ff5695957dcc1224dc1799807f98fc7b69aa787
a8hok <ak90ashok@gmail.com>
2018-06-19 08:59:42 +0530

    added Jest unit test configuration

--- src/server/web.server.js
+++ src/server/web.server.js
@@ -5,6 +5,8 @@ class WebServer {
     this.app = express();
     this.app.use(express.static('dist/public'));
   }
+
+  // To start web server.
   start() {
     return new Promise((resolve, reject) => {
       try{
@@ -15,8 +17,23 @@ class WebServer {
         console.error(e);
         reject(e);
       }
-    })
+    });
   }
+
+  // To stop web server
+  stop() {
+    return new Promise((resolve, reject) => {
+      try {
+        this.server.close(() => {
+          resolve();
+        });
+      } catch(e) {
+          console.log(e);
+          reject(e);
+      }
+    });
+  }
+
 }
 
 export default WebServer;
